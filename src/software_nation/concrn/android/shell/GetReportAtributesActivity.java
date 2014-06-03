7a3df3877f4c78bfc668b3893fba57d2425c9770
Bhanu Gupta <bhanu@techilasolutions.com>
2014-06-03 15:16:24 +0530

    Update GetReportAtributesActivity.java

--- src/software_nation/concrn/android/shell/GetReportAtributesActivity.java
+++ src/software_nation/concrn/android/shell/GetReportAtributesActivity.java
@@ -1,5 +1,6 @@
 package software_nation.concrn.android.shell;
 
+import java.io.ByteArrayOutputStream;
 import java.io.File;
 import java.util.ArrayList;
 
@@ -19,12 +20,15 @@ import android.content.Context;
 import android.content.Intent;
 import android.database.Cursor;
 import android.graphics.Bitmap;
+import android.graphics.Bitmap.CompressFormat;
+import android.graphics.BitmapFactory;
 import android.net.Uri;
 import android.os.Bundle;
 import android.os.StrictMode;
 import android.provider.MediaStore;
 import android.support.v4.app.FragmentActivity;
 import android.support.v4.app.FragmentManager;
+import android.util.Base64;
 import android.util.Log;
 import android.view.View;
 import android.widget.AdapterView;
@@ -43,6 +47,11 @@ public class GetReportAtributesActivity extends FragmentActivity {
 	ArrayList<Item> settingsItems = new ArrayList<Item>();
 	Bitmap photo = null;
 	File image = null;
+	int index,top;
+	Bitmap bitmap;
+	String base69Data;
+	byte[] data;
+
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		// TODO Auto-generated method stub
@@ -52,7 +61,6 @@ public class GetReportAtributesActivity extends FragmentActivity {
 		.detectAll().penaltyLog().penaltyDeath().build());
 		StrictMode.setVmPolicy(new StrictMode.VmPolicy.Builder().detectAll()
 				.penaltyLog().penaltyDeath().build());
-
 		/*custom list implemented so the GUI of list looks just like on iOS; 
 		 * list uses custom classes EntryItem (for the items) and Section Item (for the headings)
 		 */
@@ -62,39 +70,57 @@ public class GetReportAtributesActivity extends FragmentActivity {
 		setUpRaceItems();
 		setUpSettingsItems();
 
+
 		list = (ListView) findViewById(R.id.listView_main);
 		adapter = new EntryAdapter(GetReportAtributesActivity.this, items);
 		list.setAdapter(adapter);
 		list.setOnItemClickListener(new OnItemClickListener() {
 
 			@Override
-			public void onItemClick(AdapterView<?> arg0, View arg1, int arg2,
-					long arg3) {
+			public void onItemClick(AdapterView<?> arg0, View arg1, int arg2,long arg3) {
 				// TODO Auto-generated method stub
+
 				EntryItem item = (EntryItem) items.get(arg2);
+
 				if (item.title.equalsIgnoreCase("At risk of harm")
 						|| item.title.equalsIgnoreCase("Under the influence")
 						|| item.title.equalsIgnoreCase("Anxious")
 						|| item.title.equalsIgnoreCase("Depressed")
 						|| item.title.equalsIgnoreCase("Aggravated")
 						|| item.title.equalsIgnoreCase("Threatening"))
-					Constants.report.observations = Constants.report.observations + " "+item.title;
-				updateItem(item);
-				if(item.title.equalsIgnoreCase("Gender") && item.hasNext){
-					showListSubItemsDialog(GetReportAtributesActivity.this, genderItems, "Gender");
-				}
+
+					//Constants.report.observations = Constants.report.observations + " "+item.title;
+					//indexesList.add(arg2);
+
+
+					if(item.title.equalsIgnoreCase("Gender") && item.hasNext){
+
+						showListSubItemsDialog(GetReportAtributesActivity.this, genderItems, "Gender");
+						//Constants.report.gender = Constants.report.observations + " "+item.title;
+					}
+
 				if(item.title.equalsIgnoreCase("Age Group") && item.hasNext){
+
 					showListSubItemsDialog(GetReportAtributesActivity.this, ageItems, "Age");
+					//Constants.report.observations = Constants.report.observations + " "+item.title;
 				}
 				if(item.title.equalsIgnoreCase("Race/Ethniticity") && item.hasNext){
+
 					showListSubItemsDialog(GetReportAtributesActivity.this, raceItems, "Race");
+					//Constants.report.observations = Constants.report.observations + " "+item.title;
 				}
 				if(item.title.equalsIgnoreCase("Crisis Setting") && item.hasNext){
+
 					showListSubItemsDialog(GetReportAtributesActivity.this, settingsItems, "Settings");
+					//Constants.report.observations = Constants.report.observations + " "+item.title;
 				}
 				if(item.title.equalsIgnoreCase("Incident Picture")){
+
 					startCameraActivity();
 				}
+
+				storePosition();
+				updateItem(item);
 			}
 
 		});
@@ -118,7 +144,7 @@ public class GetReportAtributesActivity extends FragmentActivity {
 		items.add(new SectionItem("Additional Description..."));
 		items.add(new EntryItem("Incident Picture", true, false, "Additionals"));		
 	}
-	
+
 	/*method for getting image using Camera*/
 	protected void startCameraActivity() {
 		Intent cameraIntent = new Intent(android.provider.MediaStore.ACTION_IMAGE_CAPTURE); 
@@ -129,10 +155,15 @@ public class GetReportAtributesActivity extends FragmentActivity {
 			photo = (Bitmap) data.getExtras().get("data"); 
 			Uri selectedImageUri1 = data.getData();
 			String path = getRealPathFromURI(GetReportAtributesActivity.this, selectedImageUri1);
+			System.out.println("selected image path is : "+path);
+
 			image = new File(path);
+			/****************************/
+			System.out.println("path of file is : "+image.getPath()+"  file size is : "+image.length());
+	
 		}  
 	} 
-	
+
 	/*method for geting an image path stored on device*/
 	public String getRealPathFromURI(Context context, Uri contentUri) {
 		Cursor cursor = null;
@@ -142,7 +173,7 @@ public class GetReportAtributesActivity extends FragmentActivity {
 			int column_index = cursor.getColumnIndexOrThrow(MediaStore.Images.Media.DATA);
 			cursor.moveToFirst();
 			return cursor.getString(column_index);
-		} finally {
+		}finally {
 			if (cursor != null) {
 				cursor.close();
 			}
@@ -192,10 +223,13 @@ public class GetReportAtributesActivity extends FragmentActivity {
 		else
 			item.hasChecked = true;
 		list.setAdapter(adapter);
+		/*restores the  index of ListView before updating the adapter*/
+		list.setSelectionFromTop(index, top);
 	}
 
 	public void notifyDataChanged(){
 		list.setAdapter(adapter);
+		list.setSelectionFromTop(index, top);
 	}
 
 	public void Call911(View v) {
@@ -203,12 +237,15 @@ public class GetReportAtributesActivity extends FragmentActivity {
 	}
 
 	public void UploadReport(View v) {
-		HelperActivity helper = new HelperActivity(
-				GetReportAtributesActivity.this);
+
+		HelperActivity helper = new HelperActivity(GetReportAtributesActivity.this);
 
 		/*alternative upload address*/
 		//		String url = "http://rangers.concrn.com/reports";
+
+		/*stores the lat long phone address name */
 		String url = "http://www.concrn.com/reports";
+		
 
 		int reportID = 0;
 
@@ -217,12 +254,16 @@ public class GetReportAtributesActivity extends FragmentActivity {
 		try {
 			jObj = new JSONObject(result);
 			reportID = Integer.parseInt(jObj.getString("id"));
-		} catch (JSONException e) {
+		}catch (JSONException e) {
 			e.printStackTrace();
 		}
+
+
 		if(reportID>0){
-			String result2 = helper.POST(url+"/"+reportID+"/upload", getExtendedReport().toString(), image);
-			Log.v("Result:", result2);
+			String result2 = helper.POST(url+"/"+reportID+"/upload", getExtendedReport().toString(), data);
+			Log.v("Result of basic info is :", result2);
+			System.out.println("the result of basic info is : "+result2); 
+			System.out.println("the response is : "+result2);
 			if(result2!=null){
 				String name = Constants.report.name;
 				String phone = Constants.report.phone;
@@ -243,11 +284,13 @@ public class GetReportAtributesActivity extends FragmentActivity {
 			attr.put("long", Constants.report.longitude);
 			attr.put("name", Constants.report.name);
 			attr.put("phone", Constants.report.phone);
+
 			request.put("report", attr);
 		} catch (JSONException e1) {
 			e1.printStackTrace();
 		}
 		Log.v("Result using JSON:", request.toString());
+
 		return request;
 	}
 
@@ -255,17 +298,31 @@ public class GetReportAtributesActivity extends FragmentActivity {
 		JSONObject request = new JSONObject();
 		JSONObject attr = new JSONObject();
 		try {
+			/*if(base69Data.length()>0){
+				attr.put("image", base69Data);
+			}*/
 			attr.put("age", Constants.report.age);
 			attr.put("gender", Constants.report.gender);
-			attr.put("nature", Constants.report.nature);
-			attr.put("observations", Constants.report.observations);
+			attr.put("nature", Constants.report.nature);						
+
+			for (int i = 6; i < 12; i++) {
+				EntryItem item = (EntryItem) items.get(i);
+				if(item.hasChecked)
+					Constants.report.observations = Constants.report.observations + " "+item.title;
+			}			
+
+			attr.put("observations",Constants.report.observations);		
 			attr.put("race", Constants.report.race);
 			attr.put("setting", Constants.report.setting);
+		
+		
+
 			request.put("report", attr);
 		} catch (JSONException e1) {
 			e1.printStackTrace();
 		}
 		Log.v("Result using JSON:", request.toString());
+		System.out.println("the response is : "+request.toString());
 		return request;
 	}
 	private void showCallDialog() {
@@ -274,5 +331,71 @@ public class GetReportAtributesActivity extends FragmentActivity {
 		callDialog.setCancelable(false);
 		callDialog.show(fragmentManager, "Activity");
 	}
+	/*
+	 * method stores the index of ListView first visible position    
+	 * 
+	 * */
+
+	private void storePosition() {
+		// TODO Auto-generated method stub
+		index = list.getFirstVisiblePosition();
+		View v = list.getChildAt(0);
+		top = (v == null) ? 0 : v.getTop();
+	}
+
+
+	public void decodeFile(String filePath) {
+		// Decode image size
+		BitmapFactory.Options o = new BitmapFactory.Options();
+		o.inJustDecodeBounds = true;
+		BitmapFactory.decodeFile(filePath, o);
+
+		// The new size we want to scale to
+		final int REQUIRED_SIZE = 1024;
+
+		// Find the correct scale value. It should be the power of 2.
+		int width_tmp = o.outWidth, height_tmp = o.outHeight;
+		int scale = 1;
+		while (true) {
+			
+			if (width_tmp < REQUIRED_SIZE && height_tmp < REQUIRED_SIZE)
+				break;
+			width_tmp /= 2;
+			height_tmp /= 2;
+			scale *= 2;
+		}
+
+		// Decode with inSampleSize
+		BitmapFactory.Options o2 = new BitmapFactory.Options();
+		o2.inSampleSize = scale;
+		bitmap = BitmapFactory.decodeFile(filePath, o2);
+		//base69Data=  getBase64StringData();
+
+	}
+
+	/*
+	 * 
+	 * Starting Camera 
+	 * 
+	 * */
+	/*	public void startCamera() 
+	{        
+	Intent intent = new Intent(android.provider.MediaStore.ACTION_IMAGE_CAPTURE );
+	    intent.putExtra( MediaStore.EXTRA_OUTPUT, outputFileUri );
+	    startActivityForResult( intent, 0 );
+	}*/
+	/*method to convert bitmap into a Base64 String*/
+	public void  getBase64StringData(){
+		ByteArrayOutputStream bos = new ByteArrayOutputStream();
+		bitmap.compress(CompressFormat.JPEG, 100, bos);
+		data = bos.toByteArray();
+
+		String file = Base64.encodeToString(data,Base64.DEFAULT);
+		System.out.println("the base 64  file is : "+file);
+		//return data.toString();
+	
+	}
+
 
 }
+
