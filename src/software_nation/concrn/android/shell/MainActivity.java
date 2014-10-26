c20283430a9a9c418f0bf49c052dd994447f5dbb
RobAikins <Robert Aikins>
2014-10-26 14:30:11 -0700

    Fixed posting issues

--- src/software_nation/concrn/android/shell/MainActivity.java
+++ src/software_nation/concrn/android/shell/MainActivity.java
@@ -7,6 +7,7 @@ import android.os.Bundle;
 import android.support.v4.app.FragmentActivity;
 import android.support.v4.app.FragmentManager;
 import android.telephony.PhoneNumberUtils;
+import android.util.Log;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.Toast;
@@ -26,12 +27,13 @@ public class MainActivity extends FragmentActivity {
 
 		//getting user from phone memory if exists
 		user = (User) memory.LoadPreferences(MainActivity.this, "LOGIN", User.class);
-		
 		name = (EditText) findViewById(R.id.edit_name);
 		phone = (EditText)findViewById(R.id.edit_phone);
 		if(user!=null){
 		//	name.setText(user.name);
 		//	phone.setText(user.phone);
+			Constants.report.name = user.name;
+			Constants.report.phone = user.phone;
 			helper.startActivity(MapActivity.class);
 			finish();
 		}
